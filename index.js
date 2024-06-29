dc966a28471d4bf95c0cf486612630f6ff7e3fef
silly_silvy <silvyespindola2004@gmail.com>
2024-06-29 18:05:41 -0300

    criação da rota de criação de jogo (POST) e rota de deleção de jogo (DELETE).

--- index.js
+++ index.js
@@ -49,6 +49,43 @@ app.get("/games/:id", (req,res) => {
     }
 });
 
+app.delete("/game/:id", (req,res) => {
+    if(isNaN(req.params.id)){
+        console.log("teste1");
+        res.sendStatus(400);
+    }else{
+        var id = parseInt(req.params.id);
+        console.log(id);
+        var index = DB.games.findIndex(game => game.id == id);
+        console.log(index);
+        
+        if(index === -1){
+            console.log("teste2");
+            res.sendStatus(404);
+        }else{
+            console.log("teste3");
+            DB.games.splice(index, 1);
+            res.sendStatus(200);
+        }
+
+    }
+});
+
+app.post("/game/:id", (req,res) => {
+    var { title, price, year } = req.body;
+    if((isNaN(price) || isNaN(year)) || (title === undefined || title === '') || (price === undefined || year === undefined)){
+        res.sendStatus(400);
+    }else{
+        DB.games.push({
+            id: 232,
+            title,
+            price,
+            year
+        });
+        res.sendStatus(200);
+    }
+});
+
 app.listen(3000, () => {
     console.log('rodô');
 });
